2cd974dd89205ada99585ac01d3afc6f53d63989
Karol Lasończyk <karol.lasonczyk@gmail.com>
2013-12-22 19:12:47 +0100

    Added optimalization to posix compilation (O2)

--- tests/posix.mk
+++ tests/posix.mk
@@ -44,7 +44,7 @@ OBJDIR = $(BUILDDIR)/obj
 LSTDIR = $(BUILDDIR)/lst
 
 # Define optimisation level here
-OPT = -ggdb -fomit-frame-pointer -std=gnu99
+OPT = -O2 -ggdb -fomit-frame-pointer -std=gnu99
 
 LLIBDIR  = $(patsubst %,-L%,$(DLIBDIR) $(ULIBDIR))
 IINCDIR  = $(patsubst %,-I%,$(INCDIR) $(DINCDIR) $(UINCDIR))
